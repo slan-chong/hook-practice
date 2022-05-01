b76fd3b9f831b170e53b0587e37c64a25f49ecfe
Slan <snchong.slan@gmail.com>
2022-05-01 21:49:04 +0800

    NameList&Search

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ListContext } from "../App";
+import { v4 as uuidv4 } from "uuid";
 
 const NameList = () => {
-  return <div>NameList</div>;
+  const { nameList } = useContext(ListContext);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <>
+      <input
+        onChange={(event) => setSearchTerm(event.target.value)}
+        type="text"
+        placeholder="Search..."
+      />
+      {nameList && (
+        <div>
+          {nameList.map((name) => {
+            if (searchTerm === "") {
+              return <p key={uuidv4()}>{name}</p>;
+            } else if (name.includes(searchTerm)) {
+              return <p key={uuidv4()}>{name}</p>;
+            }
+          })}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default NameList;
